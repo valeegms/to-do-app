51f6f82c0fd81b584fc2385251bb4e0a96ea0cd1
Vale González <valeysa30@hotmail.com>
2024-12-10 03:01:54 -0600

    (config): update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/to-do-app",
+   output: "export"
 };
 
 export default nextConfig;
